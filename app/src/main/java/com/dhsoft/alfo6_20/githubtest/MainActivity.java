ab6f7873fc22006b5f217634aed3d29da1e77b04
dh <dh@naver.com>
2018-04-26 14:34:39 +0900

    first commit

--- app/src/main/java/com/dhsoft/alfo6_20/githubtest/MainActivity.java
+++ app/src/main/java/com/dhsoft/alfo6_20/githubtest/MainActivity.java
@@ -2,12 +2,20 @@ package com.dhsoft.alfo6_20.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv=findViewById(R.id.tv);
+        tv.setText("Hello World");
     }
 }
